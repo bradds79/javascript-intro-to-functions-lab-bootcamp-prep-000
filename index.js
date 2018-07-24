4c3aac4a57504c79fddfc1d389c7deaa35b3fe18
griffen.bradds@gmail.com <griffen.bradds@gmail.com>
2018-07-24 22:32:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,18 +28,3 @@ function sayHiToGrandma(string) {
     console.log('string = "I love you, Grandma."')
     return "I love you, too."
 }
-
-function sayHiToGrandma(string) {
-  console.log(string.toLowerCase())
-  return "I can't hear you!"
-}
-
-function sayHiToGrandma(string) {
-    console.log('string.toUpperCase()')
-    return "YES INDEED!"
-}
-  
-function sayHiToGrandma(string) {
-    console.log('string = "I love you, Grandma."')
-    return "I love you, too."
-}
