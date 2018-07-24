0130c3b516d22462acff6d732b7140493d4216f5
griffen.bradds@gmail.com <griffen.bradds@gmail.com>
2018-07-24 22:21:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  console.log(eval(string) === eval('hello'));
+  console.log(eval(string) === 'hello');
 //  console.log(string.toLowerCase())
   return "I can't hear you!"
 }
